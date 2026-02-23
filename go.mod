79b8ccc0016c411792806e05228230bc845845ff
cfranklin121 <courtenayfranklin@gmail.com>
2026-02-22 21:07:13 -0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cfranklin121/shoot_enemy_aircraft
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int
+	x := 3
+	y := 7
+
+	sum = x + y
+	fmt.Printf("Sum of %v and %v is %v", x, y, sum)
+}
